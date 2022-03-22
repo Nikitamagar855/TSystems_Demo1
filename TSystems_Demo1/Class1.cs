67521d4fb26b17d6f3e5637d73f8a472d4f67fc6
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-22 13:38:53 +0530

    Add project files.

---
+++ TSystems_Demo1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_Demo1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Department dept = new Department();
+            dept.AssignData();
+            //  String str=dept.GetData();
+            /// Console.WriteLine(str);
+            Console.WriteLine(dept.GetData());
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ TSystems_Demo1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_Demo1
+{
+    public  class Department
+    {
+       
+        private int id;
+        private string name,location;
+
+        public void AssignData()
+        {
+            id = 1;
+            name = "Swami";
+            location = "Nashik";
+
+        }
+        public string GetData()
+        {
+            return "Dept id \n" + id +"\n"+"Name \n"+name+"\n"+"Location \n"+"\n"+location;
+            
+
+        }
+    }
+}
